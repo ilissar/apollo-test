e8cdc6dd23ede08290c78ac6202628ce865e3479
Vladyslav Morozov <ilissar@hotmail.com>
2022-06-09 16:17:14 +0300

    assignment progress

--- caesar/functions/Caesar-Split/cloud_function.py
+++ caesar/functions/Caesar-Split/cloud_function.py
@@ -1,6 +1,9 @@
+
 def cloud_function(json_input):
+    print("cloud_function.py")
+    print(json_input)
     # original - string that have to be broken into array
-    original = json_input["stringToProcess"]
+    original = json_input["original"]
     # Processing
     result = list(original)
     # return the result
--- caesar/functions/Caesar-Split/main.py
+++ caesar/functions/Caesar-Split/main.py
@@ -16,10 +16,10 @@ def lambda_handler(event, context):
     # read in the args from the POST object
     json_input = json.loads(event["body"])
     res = cloud_function(json_input)
+    print("main.lambda_handler")
     print(json_input)
     print(res)
-    print(json.dumps(res))
-    return {"statusCode": 200, "body": json.dumps(res)}
+    return {"statusCode": 200, "body": res}
 
 
 ##################################################
